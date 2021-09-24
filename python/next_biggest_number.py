27ff1a4bd6ba66ba41a59f11bfae4b6ddb58dac1
Joshua Shaffer <joshuashaffer.code@gmail.com>
2021-09-24 05:19:07 -0500

    stopping commit

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -2,12 +2,57 @@
 import sys
 
 def main():
-    next_biggest_number(sys.argv[1])
+    next_biggest_number("45071")
 
 
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+
+    # turn string to integer
+    num_map = map(int, num)
+
+    # turn number to a list
+    num_list = list(num_map)
+
+    # check if numer cannot be reduced 
+    if sorted(num_list, reverse=True) == num_list:
+        return -1
+
+    test_number = 
+
+    # loop in reverse and check if any numbers can be rearranged
+    for n in reversed(range(len(num_list))):
+        # if right most digit is bigger then next digit then return a swap number 
+        # num_list[n - 1]
+        if num_list[n] > test_number:
+            
+            num_list[n], num_list[n - 1] = num_list[n - 1], num_list[n]
+            
+            return print(num_list)
+
+        
+        
+
+
+
+            #print("bigger", num_list[n], num_list[n - 1])
+        #else:
+            #print("smaller", )
+            
+        
+
+ 
+
+
+
+
+
+
+
+    
+
+
+
+   
 
 if __name__ == "__main__":
     main()
